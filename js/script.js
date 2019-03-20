944e78f1d392c38adf5a57445ae49cc6238cc1d1
Olivier <olivier.elophe@gmail.com>
2019-03-20 15:46:57 +0100

    Avec l'objet Cours

--- js/script.js
+++ js/script.js
@@ -9,21 +9,21 @@ image.addEventListener("mouseout",function(){
 });
 function valide() {
     alert("validatoin, ok !");
-
 }
 var validation = document.querySelector(".button");
 var affichage = document.querySelector(".affichage");
 validation.addEventListener("click",valide);
 // Partie 2 :
-function afficheVoiture() {
-    var résultat = "Une " + this.fabricant + " " + this.modèle
-        + " de cette année " + this.année;
+function afficheCours() {
+    var résultat = "Avec " + this.prof + " cours de " + this.cours
+        + " \n";
     console.log(résultat);
 }
-function Voiture(fabricant, modèle, année) {
-    this.fabricant = fabricant;
-    this.modele = modele;
-    this.annee = annee;
-    this.afficheVoiture = afficheVoiture;
+function Cours(prof, cours) {
+    this.prof = prof;
+    this.cours = cours;
+
+    this.afficheCours = afficheCours;
 }
-var maVoiture = new Voiture("Eagle", "Talon TSi", 1993);
+var mesCours = new Cours("Elophe", "Web");
+mesCours.afficheCours();
