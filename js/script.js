d95c9fb012bebfbb9f68ed36be7744b3d33d45a1
Olivier <olivier.elophe@gmail.com>
2019-03-20 13:43:38 +0100

    ajout du formulaire

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ image.addEventListener("mouseover",function(){
 image.addEventListener("mouseout",function(){
     description.style.display = "none";
 });
+// Partie 2 :
+function afficheVoiture() {
+    var résultat = "Une " + this.fabricant + " " + this.modèle
+        + " de cette année " + this.année;
+    console.log(résultat);
+}
+function Voiture(fabricant, modèle, année) {
+    this.fabricant = fabricant;
+    this.modele = modele;
+    this.annee = annee;
+    this.afficheVoiture = afficheVoiture;
+}
+var maVoiture = new Voiture("Eagle", "Talon TSi", 1993);
